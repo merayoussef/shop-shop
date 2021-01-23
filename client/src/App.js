f8bf2ca723f1272eeea55bc6f1fc38f052746cda
Mera Youssef <merayoussef91@hotmail.com>
2021-01-23 17:52:33 -0600

    deploy to heroku

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,8 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Nav from "./components/Nav";
 import { StoreProvider } from "./utils/GlobalState";
-import OrderHistory from "./pages/OrderHistory";
 import Success from "./pages/Success";
+import OrderHistory from "./pages/OrderHistory";
 
 const client = new ApolloClient({
   request: (operation) => {
@@ -31,16 +31,16 @@ function App() {
       <Router>
         <div>
           <StoreProvider>
-          <Nav />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Signup} />
-            <Route exact path="/success" component={Success} />
-            <Route exact path="/orderHistory" component={OrderHistory} />
-            <Route exact path="/products/:id" component={Detail} />
-            <Route component={NoMatch} />
-          </Switch>
+            <Nav />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/success" component={Success} />
+              <Route exact path="/orderHistory" component={OrderHistory} />
+              <Route exact path="/products/:id" component={Detail} />
+              <Route component={NoMatch} />
+            </Switch>
           </StoreProvider>
         </div>
       </Router>
--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,13 +1,11 @@
-import React, { useEffect } from 'react';
-import { useQuery } from '@apollo/react-hooks';
-
+import React, { useEffect } from "react";
 import ProductItem from "../ProductItem";
+import { useStoreContext } from "../../utils/GlobalState";
+import { UPDATE_PRODUCTS } from "../../utils/actions";
+import { useQuery } from '@apollo/react-hooks';
 import { QUERY_PRODUCTS } from "../../utils/queries";
-import spinner from "../../assets/spinner.gif"
-
-import { useStoreContext } from '../../utils/GlobalState';
-import { UPDATE_PRODUCTS } from '../../utils/actions';
 import { idbPromise } from "../../utils/helpers";
+import spinner from "../../assets/spinner.gif"
 
 function ProductList() {
   const [state, dispatch] = useStoreContext();
@@ -17,27 +15,20 @@ function ProductList() {
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
   useEffect(() => {
-    // if there's data to be stored
-    if (data) {
-      // let's store it in the global state object
+    if(data) {
       dispatch({
-        type: UPDATE_PRODUCTS,
-        products: data.products
-      });
-  
-      // but let's also take each product and save it to IndexedDB using the helper function 
-      data.products.forEach((product) => {
-        idbPromise('products', 'put', product);
-      });
-      // add else if to check if `loading` is undefined in `useQuery()` Hook
+           type: UPDATE_PRODUCTS,
+          products: data.products
+        });
+        data.products.forEach((product) => {
+          idbPromise('products', 'put', product);
+        });
     } else if (!loading) {
-      // since we're offline, get all of the data from the `products` store
       idbPromise('products', 'get').then((products) => {
-        // use retrieved data to set global state for offline browsing
         dispatch({
           type: UPDATE_PRODUCTS,
-          products: products
-        });
+         products: products
+       });
       });
     }
   }, [data, loading, dispatch]);
