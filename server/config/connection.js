6f5f4b73c245278275b87b4ba2c422f37933ccde
Mera Youssef <merayoussef91@hotmail.com>
2021-01-24 14:00:38 -0600

    deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://merayoussef:ilovecoding123@cluster0.b3eak.mongodb.net/shop-shop?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 });
 
 module.exports = mongoose.connection;
