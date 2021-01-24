055e5e6d3a4ffdfec8d2b3f023b170e4744f5460
Mera Youssef <merayoussef91@hotmail.com>
2021-01-23 18:10:52 -0600

    added build folder for deployment

---
+++ client/build/precache-manifest.b9dee9931131658268ed044fc8b6c492.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfde6763ee7c43987a8520f444e3db1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b985dc5f444369aef885",
+    "url": "/static/css/main.84ad2f0c.chunk.css"
+  },
+  {
+    "revision": "6789f81363b3d689a130",
+    "url": "/static/js/2.e8f36110.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.e8f36110.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b985dc5f444369aef885",
+    "url": "/static/js/main.13aab060.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
